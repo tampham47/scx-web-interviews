36604db31d8b6d82b4bed84061a3cb9628a8ae15
Tam Pham <tampham47@live.com>
2024-02-02 11:53:49 +0700

    feat: add material ui

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Button } from "@mui/material";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <div>
+        <Button variant="contained">Button</Button>
+      </div>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
